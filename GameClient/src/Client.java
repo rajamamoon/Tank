b4c2fafa1f8188f6c792645cc64f6cb2f81f3232
ASUS <ASUS@DESKTOP-CJAGO0E>
2020-04-16 03:48:18 +0400

    Added comments to Fire.java

--- GameClient/src/Client.java
+++ GameClient/src/Client.java
@@ -1,16 +1,7 @@
-import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
 import java.net.Socket;
-import java.net.SocketException;
-import java.net.UnknownHostException;
 
 public class Client {
 
