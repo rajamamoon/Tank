88ca2e6709b0143fbf2ba2bad0c669c4605f4d70
ASUS <ASUS@DESKTOP-CJAGO0E>
2020-04-16 03:59:32 +0400

    Added comments to Client.java

--- GameClient/src/Client.java
+++ GameClient/src/Client.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class Client {
-
+   //initialize the variables 
     private Socket clientSocket;
     private String hostName;
     private int serverPort;
@@ -15,34 +15,35 @@ public class Client {
     private static Client client;
     
     
-    private String message="";
+    private String message=""; 
   
     private Client() throws IOException 
     {
         protocol=new Protocol();
     }
 
-    public void register(String Ip,int port,int posX,int posY) throws IOException
+    public void register(String Ip,int port,int posX,int posY) throws IOException // register for new connection usign the arguments
     {
-        this.serverPort=port;
-        this.hostName=Ip;
-        clientSocket=new Socket(Ip,port);
-        writer=new DataOutputStream(clientSocket.getOutputStream());
-        writer.writeUTF(protocol.RegisterPacket(posX,posY));
+        this.serverPort=port; // gets the arguments from teh fucntion and save it to port
+        this.hostName=Ip;    // gets the arguments from teh fucntion and save it to Ip
+        clientSocket=new Socket(Ip,port); // establish a connection on the specified socket
+        writer=new DataOutputStream(clientSocket.getOutputStream());  // initialise the writer from the socket
+        writer.writeUTF(protocol.RegisterPacket(posX,posY)); // write the X and Y position of the tank to the output stream
         
     }
   
     public void sendToServer(String message)
     {   
-        if(message.equals("exit"))
-            System.exit(0);
-        else
+        if(message.equals("exit")) // if the message frm the client is eqaual to exit
+            System.exit(0); // close the GUI
+        else // if not
         {
-             try {
-                 Socket s=new Socket(hostName,serverPort);
-                 System.out.println(message);
-                 writer=new DataOutputStream(s.getOutputStream());
-                writer.writeUTF(message);
+             try //try this
+             {
+                 Socket s =new Socket(hostName,serverPort); // make a new socket connection to the IP and Port
+                 System.out.println(message); 
+                 writer=new DataOutputStream(s.getOutputStream()); // start a dataoutput stream
+                writer.writeUTF(message); //write the exit message to the server
             } catch (IOException ex) {
 
             }
@@ -50,6 +51,7 @@ public class Client {
 
     }
     
+    // Getter for Socket and IP
     public Socket getSocket()
     {
         return clientSocket;
@@ -58,17 +60,19 @@ public class Client {
     {
         return hostName;
     }
+    // class for initializing the client
     public static Client getGameClient()
     {
-        if(client==null)
+        if(client==null) //if there is no client
             
             try {
-                client=new Client();
+                client=new Client(); // start a new client 
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
         return client;
     }
+    // Close all connection and writer and reader
     public void closeAll()
     {
         try {
