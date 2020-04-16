addc286a5c48817fff76e3d3cce8d3cf37cf598c
ASUS <ASUS@DESKTOP-CJAGO0E>
2020-04-16 14:02:12 +0400

    Added comments to GameBoardPanl.java

--- GameClient/src/GameBoardPanel.java
+++ GameClient/src/GameBoardPanel.java
@@ -10,6 +10,7 @@ public class GameBoardPanel extends JPanel {
     
     /** Creates a new instance of GameBoardPanel */
     private Tank tank;
+    //size of the gameboard panel
     private int width=1200;
     private int height=923;
     private static ArrayList<Tank> tanks;
@@ -23,16 +24,17 @@ public class GameBoardPanel extends JPanel {
         setBounds(-50,0,width,height);
         addKeyListener(new InputManager(tank));
         setFocusable(true);
-       
+        //spawing limited amount fo tanks only
         tanks=new ArrayList<Tank>(100);
         
         for(int i=0;i<100;i++)
         {
             tanks.add(null);
         }
-   
     }
+    //paint class used for drawing to the gameboard panel
     public void paintComponent(Graphics gr) {
+        
         super.paintComponent(gr);
         Graphics2D g =(Graphics2D)gr;            
         g.setColor(Color.DARK_GRAY);
@@ -43,31 +45,32 @@ public class GameBoardPanel extends JPanel {
         g.setColor(Color.RED);
         g.setFont(new Font("Comic Sans MS",Font.BOLD,25));
         g.drawString("Battle Tank 2D",500,30);
+        //If teh game is running
         if(gameStatus) 
         { 
-            g.drawImage(tank.getBuffImage(),tank.getXposition(),tank.getYposition(),this);
+            g.drawImage(tank.getBuffImage(),tank.getXposition(),tank.getYposition(),this);// draw the tank at the given x and y position
             for(int j=0;j<1000;j++)
             {
-                if(tank.getBomb()[j]!=null) 
+                if(tank.getBomb()[j]!=null) //if the user shots
                 {
                     if(tank.getBomb()[j].stop==false){
-                        g.drawImage(tank.getBomb()[j].getBomBufferdImg(),tank.getBomb()[j].getPosiX(),tank.getBomb()[j].getPosiY(),this);
+                        g.drawImage(tank.getBomb()[j].getBomBufferdImg(),tank.getBomb()[j].getPosiX(),tank.getBomb()[j].getPosiY(),this); //draw the shot image on the x and y position adn directoin
                     }
                 }
             }
             for(int i=1;i<tanks.size();i++) 
             {
                 if(tanks.get(i)!=null)
-                    g.drawImage(tanks.get(i).getBuffImage(),tanks.get(i).getXposition(),tanks.get(i).getYposition(),this);
+                    g.drawImage(tanks.get(i).getBuffImage(),tanks.get(i).getXposition(),tanks.get(i).getYposition(),this); //draw the tank image on the x and y position adn directoin
                 
                 for(int j=0;j<1000;j++)
                 {
                     if(tanks.get(i)!=null)
                     {
-                        if(tanks.get(i).getBomb()[j]!=null) 
+                        if(tanks.get(i).getBomb()[j]!=null)  //if the enemy shots
                         {
                             if(tanks.get(i).getBomb()[j].stop==false){
-                            g.drawImage(tanks.get(i).getBomb()[j].getBomBufferdImg(),tanks.get(i).getBomb()[j].getPosiX(),tanks.get(i).getBomb()[j].getPosiY(),this);
+                            g.drawImage(tanks.get(i).getBomb()[j].getBomBufferdImg(),tanks.get(i).getBomb()[j].getPosiX(),tanks.get(i).getBomb()[j].getPosiY(),this);//draw the shot image on the x and y position adn directoin
                             }
                         }
                     }
@@ -78,7 +81,7 @@ public class GameBoardPanel extends JPanel {
         
         repaint();
     }
-
+//setter and getter for Tank
     public void registerNewTank(Tank newTank)
     {
         tanks.set(newTank.getTankID(),newTank);
