f8ed16284f8b40f3b282b4d779feec08cfe6ceee
ASUS <ASUS@DESKTOP-CJAGO0E>
2020-04-16 14:25:51 +0400

    Added comments to Protocol.java

--- GameClient/src/Protocol.java
+++ GameClient/src/Protocol.java
@@ -1,35 +1,39 @@
+//messages used to send to client by storing them in protocol
 public class Protocol {
 
     private String message="";
-    public Protocol() 
-    {
-        
-    }
     
+    //compose a message for new tank
     public String RegisterPacket(int x,int y)
     {
         message="Hello"+x+","+y;
         return message;
     }
+    //update the position of the tank 
     public String UpdatePacket(int x,int y,int id,int dir)
     {
         message="Update"+x+","+y+"-"+dir+"|"+id;
         return message;
     }
+    //Notify the server that the user shot
     public String ShotPacket(int id)
     {
         message="Shot"+id;
         return message;
     }
-    
+    //Remove the tank 
     public String RemoveClientPacket(int id)
     {
         message="Remove"+id;
         return message;
     }
+    
     public String ExitMessagePacket(int id)
     {
         message="Exit"+id;
         return message;
+    }
+       public Protocol() 
+    {        
     }
 }
